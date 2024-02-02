9bbcadbede4d78860495de3ddc1d88c0afac8031
Atef Gaieb <gaiebatef@gmail.com>
2024-02-02 16:01:44 +0100

    modif branch forum

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let produits 
+let drinkname = document.getElementById("drinkname");
+let quantity = document.getElementById("quantity");
+let purchasePrice = document.getElementById("purchasePrice");
+let marginHT = document.getElementById("marginHT");
+let sellingPrice = document.getElementById("sellingPrice");
+let category = document.getElementById("category");
+let alcoholLevels = document.getElementById("alcoholLevels");
+let listproduct = [];
+
+
+
