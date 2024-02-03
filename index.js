00ed8465279c5de012c99ef96f19eeb319c290f8
MarianneM06 <merrymattei2b@gmail.com>
2024-02-03 22:46:28 +0100

    fonction pour afficher alcool lvl

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let marginHT = document.getElementById("marginHT");
 let sellingPrice = document.getElementById("sellingPrice");
 let category = document.getElementById("category");
 let alcoholLevels = document.getElementById("alcoholLevels");
+let labelalcoholLvl = document.getElementById("labelalcoholLvl");
 let listproduct;
 let submitButton = document.querySelector(".submitButton");
 
@@ -14,7 +15,7 @@ if (JSON.parse(localStorage.getItem("listproduct"))) {
    listproduct = [];
 }
 
-
+displayAlcoolLvl();
 
 // Creation de l'evenement click sur boutton du form 
 submitButton.addEventListener("click", (event) => {
@@ -22,7 +23,7 @@ submitButton.addEventListener("click", (event) => {
     event.preventDefault()
 
     // on ajoute a info prduit (InfoProduct) les valeurs récupérées du form
-    let infoProduct = new InfoProduct(drinkname.value, quantity.value, purchasePrice.value, marginHT.value, sellingPrice.value, category.value);
+    let infoProduct = new InfoProduct(drinkname.value, quantity.value, purchasePrice.value, marginHT.value, sellingPrice.value, category.value, alcoholLevels.value);
     // on appelle la fonction InfoProduct dans notre evenement "click" avec les valeurs récupérées de la varible (let infoProduct)
     InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice, category);
     // on l'ajoute dans le tableau listProduct
@@ -30,17 +31,33 @@ submitButton.addEventListener("click", (event) => {
     
     localStorage.setItem("listproduct", JSON.stringify(listproduct));
 
-   
+    displayAlcoolLvl();
     
 });
 
 // Creation fonction constructeur info produits
-function InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice, category) {
+function InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice, category, alcoholLevels) {
     this.nom = drinkname;
     this.quantite = quantity;
     this.pricePurchase = purchasePrice;
     this.marginHt = marginHT;
     this.priceSelling = sellingPrice;
     this.categorie = category;
+    this.alcoholLvl = alcoholLevels;
 };
+
+
+function displayAlcoolLvl() {
+    
+    if(category.value != "Alcool") {
+        alcoholLevels.style.display = "none";
+        labelalcoholLvl.style.display = "none";
+    } else if(category.value === "Alcool") {
+        alcoholLevels.style.display = "block"
+        labelalcoholLvl.style.display = "block";
+        labelalcoholLvl.style.textAlign = "center";
+    }
+}
+
+
 
