7fff4a314398d6c13690f21f66943ae908352a16
MarianneM06 <merrymattei2b@gmail.com>
2024-02-04 00:25:19 +0100

    alert valeur < 0

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ let labelalcoholLvl = document.getElementById("labelalcoholLvl");
 let listproduct;
 let submitButton = document.querySelector(".submitButton");
 
+// si la liste de produit existe alors on l'affiche sur le local storage
 if (JSON.parse(localStorage.getItem("listproduct"))) {
     listproduct = JSON.parse(localStorage.getItem("listproduct"));
 } else {
    listproduct = [];
 }
 
+// on cache le selecteur degré d'alcool 
 displayAlcoolLvl();
 
 // Creation de l'evenement click sur boutton du form 
@@ -26,11 +28,17 @@ submitButton.addEventListener("click", (event) => {
     let infoProduct = new InfoProduct(drinkname.value, quantity.value, purchasePrice.value, marginHT.value, sellingPrice.value, category.value, alcoholLevels.value);
     // on appelle la fonction InfoProduct dans notre evenement "click" avec les valeurs récupérées de la varible (let infoProduct)
     InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice, category);
-    // on l'ajoute dans le tableau listProduct
-    listproduct.push(infoProduct);
     
-    localStorage.setItem("listproduct", JSON.stringify(listproduct));
-
+    // alert si valeur inferieur à 0  
+    if(quantity.value < 0 || purchasePrice.value < 0 || marginHT.value < 0 || sellingPrice.value < 0 ) {
+        alert("Vous devez saisir un nombre supérieur à 0")
+    } else {
+        listproduct.push(infoProduct);
+        // on l'ajoute dans le tableau listProduct
+        localStorage.setItem("listproduct", JSON.stringify(listproduct));
+    }
+    
+     // on affiche le selecteur du degré d'alcool
     displayAlcoolLvl();
     
 });
@@ -39,14 +47,14 @@ submitButton.addEventListener("click", (event) => {
 function InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice, category, alcoholLevels) {
     this.nom = drinkname;
     this.quantite = quantity;
-    this.pricePurchase = purchasePrice;
+    this.prixAchat = purchasePrice;
     this.marginHt = marginHT;
-    this.priceSelling = sellingPrice;
+    this.prixDevente = sellingPrice;
     this.categorie = category;
     this.alcoholLvl = alcoholLevels;
 };
 
-
+// creation de la fonction pour afficher le degré d'alcool si c'est une boisson alcoolisée
 function displayAlcoolLvl() {
     
     if(category.value != "Alcool") {
@@ -59,5 +67,12 @@ function displayAlcoolLvl() {
     }
 }
 
+// if(quantity.value < 0 || purshasePrice.value < 0 || marginHT.value < 0 || sellingPrice.value < 0 ) {
+//     submitButton.addEventListener("error", (e) => {
+//         alert("Le chiffre ne peux être inferieur à 0");
+//     })
+// } else {
+
+// }
 
 
