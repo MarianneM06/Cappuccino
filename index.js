89dad7d823d536e38146ce7b41f7f79f77e8a6f0
MarianneM06 <merrymattei2b@gmail.com>
2024-02-03 16:29:01 +0100

    local storage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let drinkname = document.getElementById("drinkname");
+let drinkname = document.querySelector("#drinkName");
 let quantity = document.getElementById("quantity");
 let purchasePrice = document.getElementById("purchasePrice");
 let marginHT = document.getElementById("marginHT");
@@ -7,15 +7,14 @@ let category = document.getElementById("category");
 let alcoholLevels = document.getElementById("alcoholLevels");
 let listproduct;
 let submitButton = document.querySelector(".submitButton");
-let listSaved = JSON.parse(localStorage.getItem("listproduct"));
 
 if (JSON.parse(localStorage.getItem("listproduct"))) {
-     listproduct = JSON.parse(localStorage.getItem("listproduct"));
+    listproduct = JSON.parse(localStorage.getItem("listproduct"));
 } else {
-    listproduct = [];
+   listproduct = [];
 }
 
-// FAIRE UN FOR EACH POUR RECUPERER LA VELEUR DU SELECT ?????
+
 
 // Creation de l'evenement click sur boutton du form 
 submitButton.addEventListener("click", (event) => {
@@ -28,9 +27,12 @@ submitButton.addEventListener("click", (event) => {
     InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice, category);
     // on l'ajoute dans le tableau listProduct
     listproduct.push(infoProduct);
+    
+    localStorage.setItem("listproduct", JSON.stringify(listproduct));
 
-
-})
+   
+    
+});
 
 // Creation fonction constructeur info produits
 function InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice, category) {
@@ -41,3 +43,4 @@ function InfoProduct(drinkname, quantity, purchasePrice, marginHT, sellingPrice,
     this.priceSelling = sellingPrice;
     this.categorie = category;
 };
+
