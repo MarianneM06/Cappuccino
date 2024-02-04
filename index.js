aa5b739bcedd8301b92afc9c89d662c3d78d458f
Atef Gaieb <gaiebatef@gmail.com>
2024-02-04 16:29:46 +0100

    Atef

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ let marginHT = document.getElementById("marginHT");
 let sellingPrice = document.getElementById("sellingPrice");
 let category = document.getElementById("category");
 let alcoholLevels = document.getElementById("alcoholLevels");
-let listproduct = [];
+
+let listproducts = [];
+
+
+let listSaved = JSON.parse(localStorage.getItem("listProduits"));
+
+if (listSaved) {
+    listproducts = listSaved;
+} else {
+    listproducts = [];
+}
+
+function stopVideoAfterFirstPlay() {
+    let logoClip = document.querySelector(".logoClip");
+    video.removeEvenListener(`ended`, stopVideoAfterFirstPlay);
+    video.pause();
+}
+
+
 
 
 
