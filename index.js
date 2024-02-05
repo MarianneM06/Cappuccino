f1f6ffa1bc0d1b02c4af4f950f93f4c1862b623f
Atef Gaieb <gaiebatef@gmail.com>
2024-02-05 01:02:16 +0100

    js avancement

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-let drinkname = document.getElementById("drinkname");
-let quantity = document.getElementById("quantity");
-let purchasePrice = document.getElementById("purchasePrice");
-let marginHT = document.getElementById("marginHT");
-let sellingPrice = document.getElementById("sellingPrice");
-let category = document.getElementById("category");
-let alcoholLevels = document.getElementById("alcoholLevels");
+let drinkname = document.getElementById(".drinkname");
+let quantity = document.getElementById(".quantity");
+let purchasePrice = document.getElementById(".purchasePrice");
+let marginHT = document.getElementById(".marginHT");
+let sellingPrice = document.getElementById(".ellingPrice");
+let category = document.getElementById(".category");
+let alcoholLevels = document.getElementById(".alcoholLevels");
 
 let submitButton = document.querySelector(".submitButton");
-let detailsButton = document.querySelector("detailsButton");
+let detailsButton = document.querySelector(".detailsButton");
 
 let listproducts;
-
-
-let listSaved = JSON.parse(localStorage.getItem("listProduits"));
-
-if (listSaved) {
-    listproducts = listSaved;
-} else {
-    listproducts = [];
-}
 
 function stopVideoAfterFirstPlay() {
     let logoClip = document.querySelector(".logoClip");
@@ -26,7 +17,38 @@ function stopVideoAfterFirstPlay() {
     video.pause();
 }
 
+if (JSON.parse(localStorage.getItem("lisproducts"))) {
+    contactArray = JSON.parse(localStorage.getItem("contactArray"));
+    renderContact(listproducts);
+} else {
+    listproducts = [];
+}
 
+submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let product = {
+        drinkname: drinkname.value,
+        quantity: quantity.value,
+        purchasePrice: purchasePrice.value,
+        marginHT: marginHT.value,
+        sellingPrice: sellingPrice.value,
+        category: category.value,
+        alcoholLevels: alcoholLevels.value,
+    }
+
+    listproducts.push(product);
+
+    localStorage.setItem("lisproduct", JSON.stringify(listproducts));
+    renderContact(listproducts);
+})
+
+function renderContact(array) {
+    array.forEach(function (product, index) {
+        li = li + `<li> ${product.drinkname} ${product.quantity} $`
+        
+    });
+}
 
 
 
